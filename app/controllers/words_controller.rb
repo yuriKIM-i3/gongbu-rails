e96d85d04c222d91ff39f7b9f518f6800e94d4a1
crane93 <youl9094@gmail.com>
2023-03-06 07:19:42 -0500

    Wordコントローラ実装

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,2 +1,34 @@
 class WordsController < ApplicationController
+
+  def index
+    @words = Word.all
+  end
+
+  def show
+    @word = Word.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    Word.save(word_params)
+  end
+
+  def edit
+  end
+
+  def update
+    @word.update(word_params)
+  end
+
+  def destroy
+    Word.find(params[:id]).destroy
+  end
+
+  private
+
+    def word_params
+      params.require(:word).permit(:vocabulary, :pronunciation, :meaning, :example)
+    end
 end
