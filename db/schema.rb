e4e5ffed9a28d6edcf680ca0a0f568fdf2ead8ae
crane93 <youl9094@gmail.com>
2023-03-13 08:40:02 -0400

    단어테이플을 유저테이플에 릴레이션쉽 가지게 함

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_120359) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_120131) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "password"
@@ -27,7 +27,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_120359) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "views"
-    t.integer "user_id"
   end
 
 end
